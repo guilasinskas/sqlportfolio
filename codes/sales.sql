f5392b45b8404168ac348177b3d2226ec3234025
Guilherme Lasinskas <37820933+guilasinskas@users.noreply.github.com>
2023-07-05 17:45:52 -0300

    Update sales.sql

--- codes/sales.sql
+++ codes/sales.sql
@@ -1,21 +1,21 @@
-\* 
-  Neste projeto fictício, atuo como analista de dados na área de vendas B2B de uma empresa.
-  Meu objetivo é realizar uma análise detalhada dos dados de vendas disponíveis e gerar insights
-  valiosos para impulsionar o negócio. Utilizarei consultas em SQL para explorar os dados,
-  identificar tendências, padrões e fazer recomendações para a equipe de vendas com base
-  nos resultados obtidos.
-*/ 
+/*
+    Neste projeto fictício, atuo como analista de dados na área de vendas B2B de uma empresa.
+    Meu objetivo é realizar uma análise detalhada dos dados de vendas disponíveis e gerar insights
+    valiosos para impulsionar o negócio. Utilizarei consultas em SQL para explorar os dados,
+    identificar tendências, padrões e fazer recomendações para a equipe de vendas com base
+    nos resultados obtidos.
+*/
   
-\* Conjunto de Dados:
-O conjunto de dados disponível contém as seguintes informações:
-- Data da venda
-- SKU do produto
-- Valor da venda
-- Quantidade vendida
-- Canal de venda
-- Cliente
-- Região geográfica
-- Categoria do produto
+/*
+    O conjunto de dados disponível contém as seguintes informações:
+    - Data da venda
+    - SKU do produto
+    - Valor da venda
+    - Quantidade vendida
+    - Canal de venda
+    - Cliente
+    - Região geográfica
+    - Categoria do produto
 */ 
 
   
@@ -24,8 +24,9 @@ SELECT canal_de_venda, SUM(valor_da_venda) AS total_vendas
 FROM tabela_de_vendas
 GROUP BY canal_de_venda
 ORDER BY total_vendas DESC;
-\* Com base nessa consulta, identificaremos os canais de vendas que mais
-  contribuem para a receita da empresa.
+/*
+    Com base nessa consulta, identificaremos os canais de vendas que mais
+    contribuem para a receita da empresa.
 */ 
 
   
@@ -34,8 +35,9 @@ SELECT EXTRACT(MONTH FROM data_da_venda) AS mes, SUM(valor_da_venda) AS total_ve
 FROM tabela_de_vendas
 GROUP BY mes
 ORDER BY mes;
-\* Essa consulta nos permitirá visualizar os padrões sazonais de vendas ao longo
-  do ano.
+/*
+    Essa consulta nos permitirá visualizar os padrões sazonais de vendas ao longo
+    do ano.
 */ 
 
   
@@ -44,8 +46,9 @@ SELECT regiao_geografica, SUM(valor_da_venda) AS total_vendas
 FROM tabela_de_vendas
 GROUP BY regiao_geografica
 ORDER BY total_vendas DESC;
-\* Ao analisar as vendas por região geográfica, identificaremos as áreas de maior potencial
-  de crescimento e adaptaremos as estratégias de vendas específicas para cada região.
+/* 
+    Ao analisar as vendas por região geográfica, identificaremos as áreas de maior potencial
+    de crescimento e adaptaremos as estratégias de vendas específicas para cada região.
 */ 
 
   
@@ -55,6 +58,7 @@ FROM tabela_de_vendas
 GROUP BY sku_do_produto
 ORDER BY total_vendas DESC
 LIMIT 5;
-\* Com essa consulta, identificaremos os produtos mais vendidos e focaremos em estratégias
-  para aumentar ainda mais sua demanda.
+/* 
+    Com essa consulta, identificaremos os produtos mais vendidos e focaremos em estratégias
+    para aumentar ainda mais sua demanda.
 */ 
